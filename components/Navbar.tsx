f39d73089186a8ecfa974b4abdc99bc69b9e1df5
Natalie Cervantes <natalie.cervantes@gmail.com>
2025-01-31 15:13:03 -0800

    fixed link back to home so that users can tap on name plus logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,11 +6,11 @@ import Navigation from './Navigation'
 const Navbar = () => {
   return (
     <div className="fixed top-0 z-[50] w-full h-[100px] bg-violet-950 flex  items-center px-10 md:px-20">
+        <Link
+            href={"/"}
+        >
         <div className="flex flex-row gap-3 items-center">
             <div className='relative w-[28px] h-[28px]'>
-                <Link
-                    href={"/"}
-                >
                 <Image
                     src="/nc-logo.png"
                     alt="logo"
@@ -18,7 +18,6 @@ const Navbar = () => {
                     height={28}
                     className="w-full h-full object-contain rounded-full"
                 />
-                </Link>
             </div>
             <h1 className="text-white text-[25px] font-semibold"> Natalie{" "}
                 <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
@@ -26,6 +25,7 @@ const Navbar = () => {
                 </span>
             </h1>
         </div>
+        </Link>
 
        <div style={{right: "5%"}}
         className="flex flex-row gap-3 items-center  w-[28px] h-[28px] bg-transparent px-10 md:px-20"
