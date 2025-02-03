1f69bcb98015f06d05f4dd04122e31e49dd3ea1e
Natalie Cervantes <natalie.cervantes@gmail.com>
2025-02-03 15:49:22 -0800

    refactored to ensure latest 4 posts are featured, and prior posts go to past posts page

--- app/utils/contentful.ts
+++ app/utils/contentful.ts
@@ -35,11 +35,10 @@ export const getNewestBlogPosts = async () => {
     const results = await client.getEntries<BlogPostFields>({
       content_type: 'blogPost',
       order: ['-sys.createdAt'], // Order by creation date, descending
-      limit: 4, // Limit to top 4 entries
       include: 1, // Include linked assets (images)
   });
  
-  const recentPosts = results.items.map((blog: Entry<BlogPostFields>) => {
+  const recentPosts = results.items.slice(0,4).map((blog: Entry<BlogPostFields>) => {
     const { blogTitle, slug, image} = blog.fields;
 
     // Custom type guard function 'isAsset' to ensure that image is valid Asset object and that it has the necessary fields and file properties.
@@ -49,15 +48,11 @@ export const getNewestBlogPosts = async () => {
     }
     
     const imageUrl = isAsset(image) ? `https:${image.fields.file?.url}`  : ''; // After verifying 'image' is a valid Asset we safely access file.url
-    
-    if (!imageUrl) {
-        throw new Error('No image URL found');
-      };
 
       return {
         blogTitle: String(blogTitle),
         slug,
-        image: imageUrl,
+        image: imageUrl || '',
       }; 
     });
     return recentPosts;
@@ -84,22 +79,14 @@ export const getPastPosts = async () => {
     order: ['-sys.createdAt'], // Order by system creation date
   });
 
-  const pastPosts = new Date();
-  pastPosts.setDate(pastPosts.getDate() - 7); // may want to update to longer time period in the future
+  // Remove the newest 4 posts from past posts
+  const pastPosts = results.items.slice(4).map((blog: Entry<BlogPostFields>) => {
+    const { blogTitle, slug } = blog.fields;
+    return {
+      blogTitle: String(blogTitle),
+      slug,
+    };
+  });
 
-  // filter only posts older than a week 
-  const archivedPosts = results.items
-    .filter((blog) => {
-      const blogDate = new Date(blog.fields.publishedDate);
-      return blogDate < pastPosts;
-    })
-    .map((blog: Entry<BlogPostFields>) => {
-      const { blogTitle, slug } = blog.fields;
-      return {
-        blogTitle: String(blogTitle),
-        slug,
-      };
-      })
-
-  return archivedPosts;
-}
+  return pastPosts;
+};
