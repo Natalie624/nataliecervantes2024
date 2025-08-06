97adaa14eae723c1d3daacfe4e478662030974ef
Natalie Cervantes <natalie.cervantes@gmail.com>
2025-08-06 13:59:10 -0700

    added privacy page and styling

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,18 +1,57 @@
-// app/terms/page.tsx
+// app/privacy/page.tsx
 
-export default function PrivacyPage() {
+import React, { ReactNode } from "react";
+import { getLegalContentBySlug } from "../utils/contentful";
+import { documentToReactComponents, Options } from "@contentful/rich-text-react-renderer";
+import { BLOCKS, INLINES, MARKS, Document, Node } from "@contentful/rich-text-types";
+
+
+
+const options: Options = {
+  renderMark: {
+    [MARKS.BOLD]: (text: ReactNode) => <b className="text-[18px] text-black">{text}</b>,
+    [MARKS.ITALIC]: (text: ReactNode) => <i className="text-black font-semibold">{text}</i>,
+  },
+  renderNode: {
+    [INLINES.HYPERLINK]: (node: Node, children: React.ReactNode) => {
+      const url = node.data.uri;
+      return (
+        <a href={url} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
+          {children}
+        </a>
+      );
+    },
+    [BLOCKS.OL_LIST]: (_node: Node, children: React.ReactNode) => (
+      <ol className="list-decimal pl-8 mb-4 text-black">{children}</ol>
+    ),
+    [BLOCKS.HEADING_1]: (_node: Node, children: React.ReactNode) => (
+      <h1 className="text-[28px] text-black">{children}</h1>
+    ),
+    [BLOCKS.HEADING_2]: (_node: Node, children: React.ReactNode) => (
+      <h2 className="text-[24px] text-black mb-4 mt-8">{children}</h2>
+    ),
+    [BLOCKS.PARAGRAPH]: (_node: Node, children: React.ReactNode) => (
+      <p className="mb-4 text-slate-800 leading-relaxed">{children}</p>
+    ),
+  },
+};
+
+const PrivacyPage = async () => {
+  const { legalHeader, legalDescription } = await getLegalContentBySlug("privacy-policy-pulsecheck");
+
   return (
     <main className="min-h-screen bg-white px-6 py-24 md:px-20 text-gray-800 pt-[120px]">
       <section className="max-w-4xl mx-auto">
-        <h1 className="text-4xl font-bold mb-6">Privacy Policy</h1>
-        <p className="mb-4">
-          This is the placeholder text for your Privacy Policy. You can update this section with details about how you collect, store, and use user data.
-          -- Use Contentful for content mamanagement of this page -- 
-        </p>
-        <p className="mb-4">
-          Be sure to include information about cookies, analytics tools, and third-party services if applicable.
-        </p>
+        <h1 className="text-4xl font-bold mb-6">{legalHeader}</h1>
+
+        {legalDescription.nodeType === "document" ? (
+          documentToReactComponents(legalDescription, options)
+        ) : (
+          <p>Content not available</p>
+        )}
       </section>
     </main>
   );
-}
+};
+
+export default PrivacyPage;
